ae7331dbcf3fc68f1ac6a581ffc99ef907db41c5
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-10-10 21:58:25 +1100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Box } from "@mui/material";
+
+const App = () => (
+    <div>
+        <BrowserRouter>
+            <Box sx={{backgroundColor:'#000'}}>
+                
+            </Box>
+        </BrowserRouter>
+    </div>
+)
+
+export default App
