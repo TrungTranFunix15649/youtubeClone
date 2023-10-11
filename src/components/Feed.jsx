b51f7040293bf818f35ee30071e1650a93359ebc
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-10-11 22:51:48 +1100

    navbar feed search

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import {Box, Stack, Typography} from '@mui/material'
 
 const Feed = () => {
   return (
-    <div>Feed</div>
+    <Stack sx={{flexDirection:{sx:"column", md:"row"}}}>
+      <Box sx={{height:{sx:"auto", md:"92vh"}, borderRight:"1px solid #3d3d3d",px:{sx:0, md:2}}}>
+        Sidebar
+        <Typography className="copyright" variant="body2" sx={{mr:1.5, color:'#fff'}}>
+          CopyRight 2023 Thomas Tran
+        </Typography>
+      </Box>
+    </Stack>
   )
 }
 
