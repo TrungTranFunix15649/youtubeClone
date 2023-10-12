00619cc10a92ef99ca1a373ae7f460a4362ff971
Doan Nhat Trung Tran <trungtdnfx15649@funix.edu.vn>
2023-10-12 22:23:40 +1100

    sidebar style

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,9 +2,7 @@ import { Stack } from "@mui/material"
 
 import { categories } from "../utils/constants"
 
-const selectedCategory = "New"
-
-const Sidebar = () => (
+const Sidebar = ({selectedCategory, setSelectedCategory}) => (
     <Stack
     direction="row"
     sx={{
@@ -15,13 +13,14 @@ const Sidebar = () => (
         {categories.map((category)=>(
             <button
             className="category-btn"
+            onClick={()=> setSelectedCategory(category.name)}
             style={{
                 background: category.name === selectedCategory && "#FC1503",
                 color:"white"
             }}
             key={category.name}>
                 <span style={{color:category.name=== selectedCategory ? 'white' : 'red', marginRight:'15px'}}>{category.icon}</span>
-                <span style={{opacity:category.name=== selectedCategory? "1":"0"}}>{category.name}</span>
+                <span style={{opacity:category.name=== selectedCategory? "1":"0.8"}}>{category.name}</span>
             </button>
         ))}
     </Stack>
